f46fdd8f5eb1331deabbe8762168aa0738a313f2
Saymios <daniojenezis@gmail.com>
2025-08-26 19:42:02 +0300

    add two pages, install react-router and base configure

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+
+function HomePage() {
+    return (
+        <div>
+            <h2>TITLE HEAD</h2>
+        </div>
+    )
+}
+
+
+export default HomePage
---
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -0,0 +1,14 @@
+
+
+
+function ProfilePage() {
+    return (
+        <div>
+            <h1>Profile</h1>
+            <p>ADMIN</p>
+        </div>
+    )
+}
+
+
+export default ProfilePage
