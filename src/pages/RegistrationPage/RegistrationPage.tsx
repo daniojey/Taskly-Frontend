15ef018a13a551d6ae303c2338a64a6a9d503070
saymios <daniojenezis@gmail.com>
2026-01-03 20:07:02 +0200

    The basic behavior of the registration page has been configured.

--- src/pages/RegistrationPage/RegistrationPage.tsx
+++ src/pages/RegistrationPage/RegistrationPage.tsx
@@ -1,19 +1,29 @@
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useForm } from 'react-hook-form';
 import * as yup from 'yup'
+import { api } from '../../../api';
+import { useContext } from 'react';
+import { AuthContext } from '../../AuthContext.jsx'
+import { useNavigate } from "react-router"
+
 
 interface YupFormData {
     first_name: string;
     last_name: string;
+    email: string;
     username: string;
     password: string;
+    confirmPassword: string;
 }
 
 function RegistrationPage() {
+    const { login } = useContext(AuthContext)
+    const navigate = useNavigate()
     
     const schema = yup.object({
         first_name: yup.string().max(250).required('This field is required'),
         last_name: yup.string().max(100).required('This field is required'),
+        email: yup.string().email('This field type Email').required(),
         username: yup.string().test(
             'username-format',
             'Username может содержать только буквы, цифры и символы @/./+/-/_',
@@ -23,6 +33,7 @@ function RegistrationPage() {
             }
         ).required('This field is required'),
         password: yup.string().max(100).required('This field is required'),
+        confirmPassword: yup.string().required().oneOf([yup.ref('password')], 'Passwords must match')
     })
 
     const {
@@ -33,14 +44,35 @@ function RegistrationPage() {
         resolver: yupResolver(schema)
     })
 
+    const handleSubmitForm = async (data: YupFormData) => {
+        console.log(data)
+
+        try {
+            const response =await api.post(
+                'api/v1/users/',
+                {...data},
+                {}
+            )
+
+            const result = await login(data.username, data.password)
+            if (result === true) navigate('/');
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <div className='registration-page__background'>
             <div className='registration-page__body'>
-                <form>
-                    <input type="text" id='first_name' {...register('first_name')} />
-                    <input type="text" id='last_name' {...register('last_name')} />
-                    <input type="text" id='username' {...register('username')} />
-                    <input type="text" id='password' {...register('password')} />
+                <form onSubmit={handleSubmit(handleSubmitForm)}>
+                    <div className='registration-form-field'></div>
+                    <input className='neomorphism-input' type="text" id='first_name' {...register('first_name')} />
+                    <input className='neomorphism-input'type="text" id='last_name' {...register('last_name')} />
+                    <input className='neomorphism-input'type="text" id='email' {...register('email')} />
+                    <input className='neomorphism-input'type="text" id='username' {...register('username')} />
+                    <input className='neomorphism-input'type="text" id='password' {...register('password')} />
+                    <input className='neomorphism-input'type="text" id='confirmPassword' {...register('confirmPassword')} />
+                    <button type='submit'>Submit</button>
                 </form>
             </div>
         </div>
