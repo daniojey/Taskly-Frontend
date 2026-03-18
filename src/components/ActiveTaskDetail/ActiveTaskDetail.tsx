eae5d946e16411ee031024aa6d80ca39d2652599
saymios <daniojenezis@gmail.com>
2026-03-18 10:12:47 +0200

    changes: - change styles ActiveTaskDetail - change styles TaskTimerComponent - remove onClick props from TaskTimerComponent

--- src/components/ActiveTaskDetail/ActiveTaskDetail.tsx
+++ src/components/ActiveTaskDetail/ActiveTaskDetail.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import TaskTimerComponent from "../TaskTimerComponent/TaskTimerComponent";
 import './ActiveTaskDetail.css'
 
@@ -15,14 +16,22 @@ interface TaskItem {
 
 function ActiveTaskDetail({ task, onClose}: {task: TaskItem, onClose: () => void}) {
     const {id, name, project_name, description, deadline, created_at, status} = task
+    const [isClose, setIsClose] = useState<boolean>()
 
+    const handleClose = () =>  {
+        setIsClose(true)
+
+        setTimeout(() => {
+            onClose()
+        }, 400)
+    }
 
     return (
-        <div className="detail-active-task__body">
-            <div>            
+        <div className={`detail-active-task__body ${isClose ? 'close' : ''}`}>
+            <div className="detail-active-task__content">
+                <button onClick={handleClose} className="detail-active-task__close">✖</button>
                 <div className="detail-active-task__title">
                     <h2>{name}</h2>
-                    <button onClick={onClose}>X</button>
                 </div>
 
                 <div className="detail-active-task__description">
@@ -30,18 +39,20 @@ function ActiveTaskDetail({ task, onClose}: {task: TaskItem, onClose: () => void
                     <p>{description}</p>
                 </div>
 
-                <div className="detail-active-task__session-timer">
-                    <label>Task Session</label>
-                    <h4>My sessions</h4>
-
+                <div className="detail-active-task__session-timer-wrapper">
                     <div className="detail-active-task__timer-container">
-                        <p>Session Timer</p>
+                        <h4>Session Timer</h4>
                         <TaskTimerComponent shortVersion={false} taskId={id} taskName={name}/>
+                    </div>
+                    <div className="detail-active-task__session-timer">
+                        <h4>Task Session</h4>
+                        <button>My sessions</button>
                     </div>
                 </div>
             </div>
 
             <div className="detail-active-task__data-info">
+                <h4>Deadline info</h4>
                 <p className="created_at">created: {created_at}</p>
                 <label>-</label>
                 <p className="deadline">deadline: {deadline}</p>
